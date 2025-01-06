eae8a22787d9c865e2c80d63a7a0b5ace91cfe81
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2025-01-07 01:06:55 +0500

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
-import AuthContextProvider from "./context/Auth.context";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Toaster } from './components/ui/toaster';
+import { BrowserRouter } from 'react-router';
+import AuthContextProvider from './context/Auth.context';
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthContextProvider>
       <App />
+      <Toaster/>
     </AuthContextProvider>
   </BrowserRouter>
 );
